77dd12df86e71997a5d1fa678b0e47183959a47c
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2024-08-16 03:09:45 +0800

    Add HeroIcons

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -2,8 +2,12 @@
     <nav class="bg-white border-b border-gray-200 p-4 mb-4">
         <div class="container mx-auto flex justify-center md:justify-between">
         <router-link :to="{ name: 'Home' }" class="hidden md:flex text-xl font-semibold place-items-center space-x-2">
-            <span>Todo App</span>
+            <ClipboardDocumentListIcon class="size-6 text-black mr-2" /> Todo App
         </router-link>
         </div>
     </nav>
 </template>
+
+<script setup>
+import { ClipboardDocumentListIcon } from '@heroicons/vue/24/solid'
+</script>
