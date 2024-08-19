b2295af030886d56a793554797bf00a5f162a2bb
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2024-08-20 04:13:00 +0800

    Fix seeder issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         Todo::factory()->count(7)->create(['status' => ToDoStatus::PENDING]);
-        Todo::factory()->count(3)->create(['status' => ToDoStatus::PENDING, 'due_date_at' => now()]);
         Todo::factory()->count(2)->create(['status' => ToDoStatus::IN_PROGRESS]);
         Todo::factory()->count(3)->create(['status' => ToDoStatus::COMPLETED]);
     }
